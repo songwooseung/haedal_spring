35597c105701c2f58d040cd83e16ae8e6506b3a9
songwooseung <dntmd2061@gmail.com>
2025-03-26 19:47:12 +0900

    second commit

---
+++ src/main/java/com/example/knu_haedal_springbc/KnuHaedalSpringbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.knu_haedal_springbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnuHaedalSpringbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnuHaedalSpringbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/knu_haedal_springbc/KnuHaedalSpringbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knu_haedal_springbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnuHaedalSpringbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
